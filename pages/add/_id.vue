9d5214efd53c5114133fa265fdd8f4d3a99df75f
duffte <dufftee@gmail.com>
2018-11-27 14:48:03 +0100

    asd

--- pages/add/_id.vue
+++ pages/add/_id.vue
@@ -1,46 +1,31 @@
 <template>
   <div>
-    <BaseHero 
-      :title="'Add New '+ $route.params.id" 
-      subtitle="based on defaults"/>
+    <BaseHero :title="'Add New '+ $route.params.id" 
+subtitle="based on defaults"/>
     <section class="section has-background-light">
-      <div class="container">           
+      <div class="container">
         <div class="columns">
           <div class="column">
-            <h2 class="title">
-              Add {{ $route.params.id }}
-            </h2>
-            <b-field
-              label="Permalink/id">
-              <b-input 
-                v-model="document.id" 
-                type="text"
-                placeholder="e.g. this-is-a-permalink"/>
+            <h2 class="title">Add {{ $route.params.id }}</h2>
+            <b-field label="Permalink/id">
+              <b-input v-model="document.id" 
+type="text" placeholder="e.g. this-is-a-permalink"/>
             </b-field>
-            <b-field 
-              v-for="(item, index) in data"
-              :label="item.name" 
-              :key="index"
-            >
+            <b-field v-for="(item, index) in data" 
+:label="item.name" :key="index">
               <!-- Text-->
-              <b-field 
-                v-if="item.type == 'text'" 
-              >
-                <b-input 
-                  v-model="document[item.name]" 
-                />                
+              <b-field v-if="item.type == 'text'">
+                <b-input v-model="document[item.name]"/>
               </b-field>
-              <!-- Textarea-->
+              <!-- number-->
               <b-field v-if="item.type == 'number'">
-                <b-input 
-                  v-model="document[item.name]" 
-                  type="number"/>
+                <b-input v-model="document[item.name]" 
+type="number"/>
               </b-field>
               <!-- Textarea-->
               <b-field v-if="item.type == 'textarea'">
-                <b-input 
-                  v-model="document[item.name]"  
-                  type="textarea"/>
+                <b-input v-model="document[item.name]" 
+type="textarea"/>
               </b-field>
               <!-- Topics -->
               <b-field v-if="item.type == 'array'">
@@ -48,25 +33,24 @@
                   v-model="document[item.name]"
                   ellipsis
                   icon="label"
-                  placeholder="Add an option"/>
+                  placeholder="Add an option"
+                />
               </b-field>
               <!-- Date-->
               <b-field v-if="item.type == 'date'">
-                <b-datepicker 
+                <b-datepicker
                   v-model="document[item.name]"
                   :first-day-of-week="1"
-                  placeholder="Click to select...">
-
-                  <button 
-                    class="button is-primary"
-                    @click="date = new Date()">
+                  placeholder="Click to select..."
+                >
+                  <button class="button is-primary" 
+@click="date = new Date()">
                     <b-icon icon="calendar-today"/>
                     <span>Today</span>
                   </button>
-
-                  <button 
-                    class="button is-danger"
-                    @click="date = null">
+                  
+                  <button class="button is-danger" 
+@click="date = null">
                     <b-icon icon="close"/>
                     <span>Clear</span>
                   </button>
@@ -81,42 +65,33 @@
                   type="file"
                   name="file"
                   accept="image/*"
-                  @change="detectFiles($event)" >
+                  @change="detectFiles($event)"
+                >
                 <progress
                   v-if="uploading && !uploadEnd"
-                  :value="progressUpload">
-                  {{ progressUpload }}%
-                </progress>
-                <img
-                  v-if="uploadEnd"
-                  :src="downloadURL"
-                  width="100%"
-                >
+                  :value="progressUpload"
+                >{{ progressUpload }}%</progress>
+                <img v-if="uploadEnd" 
+:src="downloadURL" width="100%">
                 <div v-if="uploadEnd">
-                  <button
-                    class="button"
-                    @click="deleteImage()"
-                  >
-                    Delete
-                  </button>
+                  <button class="button" 
+@click="deleteImage()">Delete</button>
                 </div>
                 <b-field label="Source URL">
-                  <b-input 
-                    v-model="document[item.name]"  
-                    type="text"/>
+                  <b-input v-model="document[item.name]" 
+type="text"/>
                 </b-field>
               </div>
 
               <!-- Radio -->
               <b-field v-if="item.type == 'radio'">
-                <b-switch 
+                <b-switch
                   :value="false"
                   v-model="document[item.name]"
                   type="is-info"
                   true-value="Yes"
-                  false-value="No">
-                  {{ document[item.name] }}
-                </b-switch>
+                  false-value="No"
+                >{{ document[item.name] }}</b-switch>
               </b-field>
 
               <!-- symptom -->
@@ -125,10 +100,12 @@
                   <b-autocomplete
                     v-model="selected[item.name]"
                     :data="symptom"
-                    open-on-focus="true"
+                    :open-on-focus="openOnFocus"
+                    :keep-first="keepFirst"
                     placeholder="e.g. Müdigkeit"
                     field="symptomName"
-                    @select="option => document[item.name] = option"/>
+                    @select="option => document[item.name] = option"
+                  />
                 </b-field>
               </b-field>
 
@@ -138,10 +115,12 @@
                   <b-autocomplete
                     v-model="selected[item.name]"
                     :data="wirkstoff"
-                    open-on-focus="true"
+                    :open-on-focus="openOnFocus"
+                    :keep-first="keepFirst"
                     placeholder="e.g. Zink"
                     field="wirkstoffName"
-                    @select="option => document[item.name] = option"/>
+                    @select="option => document[item.name] = option"
+                  />
                 </b-field>
               </b-field>
 
@@ -151,13 +130,14 @@
                   <b-autocomplete
                     v-model="selected[item.name]"
                     :data="wirkstoff"
-                    open-on-focus="true"
+                    :open-on-focus="openOnFocus"
+                    :keep-first="keepFirst"
                     placeholder="e.g. Zellstoffwechsel"
                     field="prozessName"
-                    @select="option => document[item.name] = option"/>
+                    @select="option => document[item.name] = option"
+                  />
                 </b-field>
               </b-field>
-
 
               <!-- Author -->
               <b-field v-if="item.type == 'author'">
@@ -165,10 +145,12 @@
                   <b-autocomplete
                     v-model="selected[item.name]"
                     :data="authors"
-                    open-on-focus="true"
+                    :open-on-focus="openOnFocus"
+                    :keep-first="keepFirst"
                     placeholder="e.g. Anne"
                     field="authorName"
-                    @select="option => document[item.name] = option"/>
+                    @select="option => document[item.name] = option"
+                  />
                 </b-field>
               </b-field>
 
@@ -178,55 +160,55 @@
                   <b-autocomplete
                     v-model="selected[item.name]"
                     :data="wirkstofftyp"
-                    open-on-focus="true"
+                    :open-on-focus="openOnFocus"
+                    :keep-first="keepFirst"
                     placeholder="e.g. Vitamin"
                     field="wirkstofftypName"
-                    @select="option => document[item.name] = option"/>
+                    @select="option => document[item.name] = option"
+                  />
                 </b-field>
-              </b-field>              
+              </b-field>
+            </b-field>
 
-            </b-field>   
-
-                     
             <br>
-            <button 
+            <button
               :disabled="writeCarSuccessful"
-              class="button is-success is-fullwidth" 
-              @click="writeCarToFirestore">
+              class="button is-success is-fullwidth"
+              @click="writeCarToFirestore"
+            >
               <span v-if="!writeCarSuccessful">Store</span>
               <span v-else>Successful!</span>
             </button>
           </div>
+
           <div class="column">
             <h1 class="title">Blocks</h1>
 
-            <ul class="">
-              <li 
-                v-for="(block, index) in blocks" 
-                :key="index"
-                class="box">
-                <div v-if="block.type == 'text'" >
-                  <h3 class="title is-5">Paragraph <button 
-                    class="delete" 
-                    @click="deleteBlock(index)">x</button></h3>
+            <ul class>
+              <li v-for="(block, index) in blocks" 
+:key="index" class="box">
+                <div v-if="block.type == 'text'">
+                  <h3 class="title is-5">
+                    Paragraph
+                    <button class="delete" 
+@click="deleteBlock(index)">x</button>
+                  </h3>
                   <label>Text</label>
                   <no-ssr>
-                    <markdown-editor 
-                      ref="markdownEditor" 
-                      v-model="block.content"/>
+                    <markdown-editor ref="markdownEditor" 
+v-model="block.content"/>
                   </no-ssr>
                   <hr>
-                </div>                    
-                <div v-if="block.type == 'image'"> 
-                  <h3 class="title is-5">Image <button 
-                    class="delete" 
-                    @click="deleteBlock(index)">x</button></h3>
+                </div>
+                <div v-if="block.type == 'image'">
+                  <h3 class="title is-5">
+                    Image
+                    <button class="delete" 
+@click="deleteBlock(index)">x</button>
+                  </h3>
                   <div>
-                    <button
-                      v-if="!uploadEnd && !uploading"
-                      @click="selectFile">
-                      Upload an image
-                    </button>
+                    <button v-if="!uploadEnd && !uploading" 
+@click="selectFile">Upload an image</button>
                     <input
                       id="files"
                       ref="uploadInput"
@@ -234,50 +216,78 @@
                       type="file"
                       name="file"
                       accept="image/*"
-                      @change="detectFiles($event)" >
+                      @change="detectFiles($event)"
+                    >
                     <progress
                       v-if="uploading && !uploadEnd"
-                      :value="progressUpload">
-                      {{ progressUpload }}%
-                    </progress>
-                    <img
-                      v-if="uploadEnd"
-                      :src="downloadURL"
-                      width="100%"
-                    >
+                      :value="progressUpload"
+                    >{{ progressUpload }}%</progress>
+                    <img v-if="uploadEnd" 
+:src="downloadURL" width="100%">
                     <div v-if="uploadEnd">
-                      <button
-                        class="button"
-                        @click="deleteImage()"
-                      >
-                        Delete
-                      </button>
+                      <button class="button" 
+@click="deleteImage()">Delete</button>
                     </div>
                   </div>
                   <label>Source</label>
-                  <input 
-                    v-model="block.src" 
-                    type="text" 
-                    class="input">
+                  <input v-model="block.src" 
+type="text" class="input">
                   <label>Caption</label>
-                  <input 
-                    v-model="block.caption" 
-                    type="text" 
-                    class="input">
+                  <input v-model="block.caption" 
+type="text" class="input">
                   <hr>
-                </div>                   
+                </div>
               </li>
             </ul>
             <hr>
             <b-field grouped>
-              <button 
-                class="button" 
-                @click="addBlock()">Add new Text</button>
-              <button 
-                class="button" 
-                @click="addImage()">Add new Image</button>
+              <button class="button" 
+@click="addBlock()">Add new Text</button>
+              <button class="button" 
+@click="addImage()">Add new Image</button>
             </b-field>
           </div>
+        </div>
+        <!-- -->
+        <div class="columns">
+          <div class="column is-6">
+            <h2 class="title">Wirkstoffe</h2>
+            <ul>
+              <li v-for="item in stoffe" 
+:key="item.id">
+                <b-field label="Wirkstoff">
+                  <b-autocomplete
+                    :data="wirkstoff"
+                    :open-on-focus="openOnFocus"
+                    :keep-first="keepFirst"
+                    placeholder="e.g. Zink"
+                    field="wirkstoffName"
+                    @select="option => item.wirkstoff = option"
+                  />
+                </b-field>
+                <b-field label="Empfehlung">
+                  <b-input v-model="item.empfehlung" 
+type="textarea"/>
+                </b-field>
+                <b-field label="So nicht">
+                  <b-input v-model="item.sonicht" 
+type="textarea"/>
+                </b-field>
+                <b-field label="Kurzinfo">
+                  <no-ssr>
+                    <markdown-editor ref="markdownEditor" 
+v-model="item.content"/>
+                  </no-ssr>
+                </b-field>
+              </li>
+            </ul>
+            <b-field grouped>
+              <button class="button" 
+@click="addWirkstoff()">+ wirkstoff</button>
+            </b-field>
+          </div>
+
+          <div class="columns">asd</div>
         </div>
       </div>
     </section>
@@ -296,6 +306,7 @@ export default {
       data: [],
       blocks: [],
       author: [],
+      stoffe: [],
       wirkstoff: [],
       symptom: [],
       wirkstofftyp: [],
@@ -391,6 +402,11 @@ export default {
         id: this.idcounter++
       })
     },
+    addWirkstoff() {
+      this.stoffe.push({
+        content: ''
+      })
+    },
     addImage() {
       this.blocks.push({ content: 'new block', type: 'image' })
     },
@@ -459,7 +475,8 @@ export default {
 
       const document = {
         data: this.document,
-        blocks: this.blocks
+        blocks: this.blocks,
+        wirkstoffe: this.stoffe
       }
 
       try {
@@ -509,17 +526,17 @@ export default {
 
     let prozessCollection = []
     let prozess = await fireDb
-      .collection('wirkstoff')
+      .collection('prozess')
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
-          wirkstoffCollection.push(doc.data().data)
+          prozessCollection.push(doc.data().data)
         })
       })
 
     let wirkstofftypCollection = []
     let wirkstofftyp = await fireDb
-      .collection('wirkstoff')
+      .collection('wirkstofftyp')
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
